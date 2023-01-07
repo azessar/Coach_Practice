7a9c4c938ea40280322d058d28493e0f74980a24
Andrew Zessar <andrewzessar@Andrews-MBP-2.lan>
2023-01-07 00:04:26 -0500

    add copy

--- frontend/src/containers/home-page/index.tsx
+++ frontend/src/containers/home-page/index.tsx
@@ -179,6 +179,129 @@ function HomePage() {
           </Box>
         </Box>
       </Box>
+      <Box
+        bgcolor={colors.primaryNavy}
+        width="80%"
+        margin="auto"
+        padding="40px"
+        marginTop={"20px"}
+        borderRadius="10px"
+        display={"flex"}
+        justifyContent="space-between"
+      >
+        <Box width="50%">
+          <Box>
+            <Text
+              words="WHO WE ARE"
+              color={colors.secondaryLightBlue}
+              fontWeight="700"
+              fontSize="36px"
+            />
+          </Box>
+          <Box marginTop={"15%"}>
+            <BigText
+              color={colors.secondaryLightBlue}
+              fontWeight="600"
+              fontSize="16px"
+              words="We are a vast network of experienced and aspiring coaching and game operations talent with a passion for teaching, playing, and growing the game, whether it's basketball, volleyball, soccer, football, or any other popular American sport.
+              "
+            />
+          </Box>
+        </Box>
+        <Box width="45%">
+          <img className="jordan-pic" src={`huddle.png`}></img>
+        </Box>
+      </Box>
+      <Box
+        bgcolor={colors.primaryNavy}
+        width="80%"
+        margin="auto"
+        padding="40px"
+        marginTop={"20px"}
+        borderRadius="10px"
+        display={"flex"}
+        justifyContent="space-between"
+      >
+        <Box width="45%">
+          <img className="jordan-pic" src={`girlcoaching.png`}></img>
+        </Box>
+        <Box width="50%">
+          <Box>
+            <Text
+              words="WHY WE DO IT"
+              color={colors.secondaryLightBlue}
+              fontWeight="700"
+              fontSize="36px"
+            />
+          </Box>
+          <Box marginTop={"10%"}>
+            <BigText
+              color={colors.secondaryLightBlue}
+              fontWeight="600"
+              fontSize="12px"
+              words="Besides a love for the game, our founding team identified a fundamental flaw in the American sports coaching industry. Finding a coach has historically been a word-of-mouth business, and we believe some of the most qualified and passionate coaching talent has been left in the dust due to lack of access to the full breadth of coaching opportunities. We are here to help the cream rise to the top by matching the best coaches with the best coaching opportunities while providing athletes the best mentorship possible."
+            />
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        bgcolor={colors.primaryNavy}
+        width="80%"
+        margin="auto"
+        padding="40px"
+        marginTop={"20px"}
+        borderRadius="10px"
+        display={"flex"}
+        justifyContent="space-between"
+      >
+        <Box width="50%">
+          <Box>
+            <Text
+              words="LOOKING FOR THE RIGHT COACH?"
+              color={colors.secondaryLightBlue}
+              fontWeight="700"
+              fontSize="24px"
+            />
+          </Box>
+          <Box marginTop={"40px"} display="flex" justifyContent="center">
+            <a
+              target="_blank"
+              href="https://docs.google.com/forms/d/e/1FAIpQLScHqK03FFN_NFOLaS474npgNPSFiVAQt66Uu2ZOabdOpmjFKw/viewform"
+            >
+              <BigText
+                color={colors.secondaryLightBlue}
+                fontWeight="600"
+                fontSize="12px"
+                words="FILL OUT THIS FORM"
+                fontStyle="underlined"
+              />
+            </a>
+          </Box>
+        </Box>
+        <Box width="50%">
+          <Box>
+            <Text
+              words="DO YOU WANT TO COACH?"
+              color={colors.secondaryLightBlue}
+              fontWeight="700"
+              fontSize="24px"
+            />
+          </Box>
+          <Box marginTop={"40px"} display="flex" justifyContent="center">
+            <a
+              target="_blank"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSe7OijdcgJGNfmG9WRWn-g3AX9JUQNAaQ1ESCCSFdwIHK9Ssg/viewform"
+            >
+              <BigText
+                color={colors.secondaryLightBlue}
+                fontWeight="600"
+                fontSize="12px"
+                words="FILL OUT THIS FORM (and then signup on this site to create a profile!)"
+              />
+            </a>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 }
